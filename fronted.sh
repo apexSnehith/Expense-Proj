1efb6126fe88ce4db93484f51a67a6537b8c0903
apexSnehith <snehith0713@gmail.com>
2023-09-09 22:25:40 +0530

    updated the frontend.sh file

--- fronted.sh
+++ fronted.sh
@@ -1,20 +1,20 @@
 echo installing nginx
-dnf install nginx -y > /tmp/expense.log
+dnf install nginx -y >> /tmp/expense.log
 
 echo placing expense config file in nginx
-cp expense.conf /etc/nginx/default.d/expense.conf > /tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf >> /tmp/expense.log
 
 echo removing default content
-rm -rf /usr/share/nginx/html/* > /tmp/expense.log
+rm -rf /usr/share/nginx/html/* >> /tmp/expense.log
 
 echo download the fronted code
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip > /tmp/expense.log
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >> /tmp/expense.log
 
 cd /usr/share/nginx/html
 
 echo extracting fronted code
-unzip /tmp/frontend.zip > /tmp/expense.log
+unzip /tmp/frontend.zip >> /tmp/expense.log
 
 echo start & enable nginx server
-systemctl enable nginx > /tmp/expense.log
-systemctl restart nginx > /tmp/expense.log
+systemctl enable nginx >> /tmp/expense.log
+systemctl restart nginx >> /tmp/expense.log
