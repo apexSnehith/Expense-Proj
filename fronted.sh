70cfd177d54c6a3abc32545d9541e35271af5afb
apexSnehith <snehith0713@gmail.com>
2023-09-10 01:15:09 +0530

    Output Redirectors

--- fronted.sh
+++ fronted.sh
@@ -1,18 +1,18 @@
 source common.sh
 component=frontend
 echo installing nginx
-dnf install nginx -y >> $log_file
+dnf install nginx -y &>>$log_file
 
 echo placing expense config file in nginx
-cp expense.conf /etc/nginx/default.d/expense.conf >> $log_file
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
 echo removing default content
-rm -rf /usr/share/nginx/html/* >> $log_file
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
 cd /usr/share/nginx/html
 
 download_and_extract
 
 echo starting Nginx server
-systemctl enable nginx >> $log_file
-systemctl restart nginx >> $log_file
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
