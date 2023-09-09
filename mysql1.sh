90b6e5da922c58eb621a5e974d880131fb3c70fe
apexSnehith <snehith0713@gmail.com>
2023-09-10 01:16:26 +0530

    Output Redirectors

--- mysql1.sh
+++ mysql1.sh
@@ -1,18 +1,18 @@
 source common.sh
 
 echo Disable MySql 8 version
-dnf module disable mysql -y >>$log_file
+dnf module disable mysql -y &>>$log_file
 
 echo Copy MySql Repo file
-cp mysql.repo /etc/yum.repos.d/mysql.repo >>$log_file
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
 
 echo Install MySql Server
-dnf install mysql-community-server -y >>$log_file
+dnf install mysql-community-server -y &>>$log_file
 
 echo Start MySql Server
-systemctl enable mysqld >>$log_file
-systemctl start mysqld >>$log_file
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
 
 echo Setup Root Password
-mysql_secure_installation --set-root-pass ExpenseApp@1 >>$log_file
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
 
