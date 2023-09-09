3df3b7d90945da26fbd1f4cf95b551845d0d7961
apexSnehith <snehith0713@gmail.com>
2023-09-09 22:35:16 +0530

    updated the backend.sh file for the rerun NFR

--- backend.sh
+++ backend.sh
@@ -13,7 +13,7 @@ curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 unzip /tmp/backend.zip
 
-cd /app
+
 npm install
 systemctl daemon-reload
 systemctl enable backend
