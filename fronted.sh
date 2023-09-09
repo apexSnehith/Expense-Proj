b857c9f219cc71257a07e5065b42c7d0ea3303b9
apexSnehith <snehith0713@gmail.com>
2023-09-10 00:29:25 +0530

    updated the fronted.sh file for the rerun NFR

--- fronted.sh
+++ fronted.sh
@@ -1,20 +1,21 @@
+log_file=/tmp/expemse.log
 echo installing nginx
-dnf install nginx -y >> /tmp/expense.log
+dnf install nginx -y >> $log_file
 
 echo placing expense config file in nginx
-cp expense.conf /etc/nginx/default.d/expense.conf >> /tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf >> $log_file
 
 echo removing default content
-rm -rf /usr/share/nginx/html/* >> /tmp/expense.log
+rm -rf /usr/share/nginx/html/* >> $log_file
 
 echo download the fronted code
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >> /tmp/expense.log
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >> $log_file
 
 cd /usr/share/nginx/html
 
 echo extracting fronted code
-unzip /tmp/frontend.zip >> /tmp/expense.log
+unzip /tmp/frontend.zip >> $log_file
 
 echo start & enable nginx server
-systemctl enable nginx >> /tmp/expense.log
-systemctl restart nginx >> /tmp/expense.log
+systemctl enable nginx >> $log_file
+systemctl restart nginx >> $log_file
