0ff6c0870dca8580b1fe4d2386bca382c9ae5ee6
apexSnehith <snehith0713@gmail.com>
2023-09-10 01:01:33 +0530

    Functions

--- common.sh
+++ common.sh
@@ -1,1 +1,9 @@
 log_file=/tmp/expense.log
+
+download_and_extract(){
+  echo download the $component code
+  curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >> $log_file
+
+  echo extracting $component code
+  unzip /tmp/$component.zip >> $log_file
+}
--- fronted.sh
+++ fronted.sh
@@ -1,5 +1,5 @@
 source common.sh
-
+component=frontend
 echo installing nginx
 dnf install nginx -y >> $log_file
 
@@ -9,13 +9,9 @@ cp expense.conf /etc/nginx/default.d/expense.conf >> $log_file
 echo removing default content
 rm -rf /usr/share/nginx/html/* >> $log_file
 
-echo download the fronted code
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >> $log_file
-
 cd /usr/share/nginx/html
 
-echo extracting fronted code
-unzip /tmp/frontend.zip >> $log_file
+download_and_extract
 
 echo starting Nginx server
 systemctl enable nginx >> $log_file
